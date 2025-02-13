6dd110e3b2f3b3883a3da1598186c7570117bac2
LasseBlochWH <blochbuster@gmail.com>
2025-02-13 21:28:05 +0100

    ci(mix): add lint mix task
    Lint is maybe not the bedst name :)

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule FocusAtWillEx.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -15,6 +16,16 @@ defmodule FocusAtWillEx.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp aliases do
+    [
+      lint: [
+        "format --check-formatted",
+        "credo",
+        "dialyzer"
+      ]
     ]
   end
 
