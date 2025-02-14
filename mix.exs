82af8789ddc05fe875d5a9f7ee0d553d76a61bf2
LasseBloch <blochbuster@gmail.com>
2025-02-14 12:40:03 +0100

    chore(depenencies): add ex_doc

--- mix.exs
+++ mix.exs
@@ -35,6 +35,7 @@ defmodule FocusAtWillEx.MixProject do
       {:jason, "~> 1.4"},
       {:styler, "~> 1.2", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:mox, "~> 1.0", only: :test},
       {:req, "~> 0.5.0"}
